5ef93e4780f239da8504ec20ab731acc16af51cd
wackytsui <allan.altavano@gmail.com>
2024-04-14 22:50:06 +0800

    Update markup-gallery.js

--- src/js/markup-gallery.js
+++ src/js/markup-gallery.js
@@ -1,32 +1,31 @@
 export default function createMarkup(films) {
-  return films
-    .map(film => {
+  return films.map(film => {
       return `
       
-      <li class="card-set__item" id="${films.id}">
-      <a href='#' id='${films.id}' class="card-link">
+      <li class="card-set__item" id="${film.id}">
+      <a href='#' id='${film.id}' class="card-link">
       <picture>
                     <source srcset="
-                    ${films.base_url}w780/${films.poster_path} 1x,
-                    ${films.base_url}original/${films.poster_path} 2x" media="(min-width: 1280px)" type="image/jpeg" />
+                    ${film.base_url}w780/${film.poster_path} 1x,
+                    ${film.base_url}original/${film.poster_path} 2x" media="(min-width: 1280px)" type="image/jpeg" />
                     <source srcset="
-                    ${films.base_url}w342/${films.poster_path} 1x,
-                    ${films.base_url}w500/${films.poster_path} 2x" media="(min-width: 768px)" type="image/jpeg" />
+                    ${film.base_url}w342/${film.poster_path} 1x,
+                    ${film.base_url}w500/${film.poster_path} 2x" media="(min-width: 768px)" type="image/jpeg" />
                     <source srcset="
-                    ${films.base_url}w185/${films.poster_path} 1x,
-                    ${films.base_url}w342/${films.poster_path} 2x" media="(max-width: 480px)" type="image/jpeg" />
-        <img id="${films.id}"
+                    ${film.base_url}w185/${film.poster_path} 1x,
+                    ${film.base_url}w342/${film.poster_path} 2x" media="(max-width: 480px)" type="image/jpeg" />
+        <img id="${film.id}"
           loading="lazy"
-          src="${films.base_url}w342/${films.poster_path}"
-          alt="${films.title}"
+          src="${film.base_url}w342/${film.poster_path}"
+          alt="${film.title}"
           class="card-set__img "/>
                 </picture>
       
     
-      <h3 class="card-set__title">${films.title}</h3>
-      <div class="card-set__description" id="${films.id}">
-      <span class="card-set__genre" id="${films.id}">
-          ${films.filmGenreList} &nbsp| ${films.releaseYear}
+      <h3 class="card-set__title">${film.title}</h3>
+      <div class="card-set__description" id="${film.id}">
+      <span class="card-set__genre" id="${film.id}">
+          ${film.filmGenreList} &nbsp| ${film.releaseYear}
       </span>
       
       </div>
